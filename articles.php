2c4b80769beab7bdc389ea26c4edb084b657a137
Paurush Sinha <paurush.sinha.d@gmail.com>
2025-11-20 01:59:51 +0530

    changes

--- articles.php
+++ articles.php
@@ -1,5 +1,5 @@
 <?php
     require_once("./utils.php");
     $db = new Database();
-    echo $db->fetchData("SELECT title as label, banner as image, slug FROM articles");
+    echo $db->fetchData("SELECT title as label, banner as image, slug FROM articles ORDER BY id DESC");
 ?>
